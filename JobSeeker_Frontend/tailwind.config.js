6d33721d9a72b2981c365a5913fb5f67b3c3211e
Prabhgun Singh <Godent@Prabhguns-MacBook.local>
2025-02-03 09:49:49 +0530

    Initial commit

---
+++ JobSeeker_Frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['newsreader', 'afacad'],
+        },
+        colors: {
+          primary: '#yourPrimaryColor',
+        },
+        spacing: {
+          sm: '8px',
+          lg: '32px',
+        },
+      },
+    },
+  };
+  
