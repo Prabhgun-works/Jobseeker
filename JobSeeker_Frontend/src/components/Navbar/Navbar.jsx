0e9936c24392e1e25764e2786f07f842d39cb2f8
Prabhgun-works <prabhgun.web@gmail.com>
2025-02-24 14:47:18 +0530

    updated home page

--- JobSeeker_Frontend/src/components/Navbar/Navbar.jsx
+++ JobSeeker_Frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   
   return (
     <div className="container">
-      <Link className="link" to="/home">Home</Link>
+      <Link className="link" to="/">Home</Link>
       <Link className="link" to="/profile">Profile</Link>
       <Link className="link" to="/login">Login</Link>
       <Link className="link" to="/signup">SignUp</Link>
